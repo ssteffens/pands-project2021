f0d2c7addad7927cb7d92713698d52472d604d0a
ssteffens <77699063+ssteffens@users.noreply.github.com>
2021-04-26 23:11:09 +0100

    add data summery txt file

--- analysis.py
+++ analysis.py
@@ -46,17 +46,17 @@ def filter_virginica(data):
 # Plot (scatter), comparing petal length and width
 def plot_data(data):
     print("plot!")
-    data.plot(kind = "scatter", x = "Petal Length", y = "Petal Width")  # https://www.w3schools.com/python/pandas/pandas_plotting.asp
+    data.plot(kind = "scatter", x = "Petal Length", y = "Petal Width")              # https://www.w3schools.com/python/pandas/pandas_plotting.asp
     plt.show()
 
 
-# Output to text file: https://izziswift.com/python-pandas-write-content-of-dataframe-into-text-file/
+# Output to text file:                                                              https://izziswift.com/python-pandas-write-content-of-dataframe-into-text-file/
 def text_output(data):
     with open ("output.txt", "wt") as f:
         f.write(data.to_string())
 
 
-# Output to text file for  "Setosa"
+# Output to text file for "Setosa"
 def text_setosa(setosa):
     with open ("setosa.txt", "wt") as f:
         #f.write(setosa.to_string())
@@ -64,18 +64,53 @@ def text_setosa(setosa):
         f.write(describe.to_string())
 
 
+# Output to text file:                                                              https://izziswift.com/python-pandas-write-content-of-dataframe-into-text-file/
+def text_overview(data):
+    with open ("datasummary.txt", "wt") as f:
+        describe_all = data.describe()
+        transp = describe_all.transpose()
+        describe_seplen = data.groupby('Species')['Sepal Length'].describe()
+        describe_sepwid = data.groupby('Species')['Sepal Width'].describe()
+        describe_petlen = data.groupby('Species')['Petal Length'].describe()
+        describe_petwid = data.groupby('Species')['Petal Width'].describe()
+
+        f.write("OVERVIEW OF IRIS DATASET\n\n")
+        f.write("Overview for all Species\n")
+        f.write(transp.to_string())                                                 # transpose the describe table so that it's aligned with the below format
+                                                                                    # source: https://note.nkmk.me/en/python-pandas-t-transpose/
+        f.write("\n\n\n\n Overview for Sepal Length by Species\n")
+        f.write(describe_seplen.to_string())
+        f.write("\n\n\n\n Overview for Sepal Width by Species\n")
+        f.write(describe_sepwid.to_string())
+        f.write("\n\n\n\n Overview for Petal Length by Species\n")
+        f.write(describe_petlen.to_string())
+        f.write("\n\n\n\n Overview for Petal Width by Species\n")
+        f.write(describe_petwid.to_string())
+
+
+def histo_all(data):
+    data.hist()
+    plt.show()
+
+        
+
 # Main program: 
 
 def main():
     data = read_data()
     #print(data)
-    #text_output(data)
-    #plot_data(data)
+    text_output(data)
+    text_overview(data)
+    plot_data(data)
     setosa = filter_setosa(data)
-    #versicolor = filter_versicolor(data)
-    #virginica = filter_virginica(data)
-    #print(virginica)
+    versicolor = filter_versicolor(data)
+    virginica = filter_virginica(data)
+    print(virginica)
+    histo_all(data)
+    
+    
     text_setosa(setosa)
+
 
 if __name__ == "__main__":
     main()
